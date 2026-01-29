ba10890f24961579acce736160db103fcd134a7a
marcoVinha <marvsdd.01@live.com>
2026-01-29 20:21:05 -0300

    chore: create component type class

---
+++ src/circuit_elements/core/base/component_type.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class ComponentType(str, Enum):
+    RESISTOR = "resistor"
+    CAPACITOR = "capacitor"
+    INDUCTOR = "inductor"
+    DIODE = "diode"
+    BJT = "bjt"
+    MOSFET = "mosfet"
+    JFET = "jfet"
+    OPAMP = "opamp"
+    TRANSFORMER = "transformer"
+    IC = "ic"
